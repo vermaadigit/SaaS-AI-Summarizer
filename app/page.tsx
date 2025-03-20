5299a11a4db86a5a24b9d110b4ea9a4c6541abfc
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-03-20 15:44:33 +0530

    Clean working root

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <></>
+    <div>
+      <Button variant={"outline"} size={"lg"}>Shadcn Button</Button>
+    </div>
   );
 }
